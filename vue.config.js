d877b87d8a57204ef63189809eea702cb2e960ad
Haodong Liu <928322528@qq.com>
2025-06-24 20:11:19 +0800

    chore: set up GitHub Pages deploy

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    devServer: {
-      open: true,
-      port: 8888
-    },
-    chainWebpack: (config) => {
-      config.module
-        .rule('glb') // 创建一个新的规则
-        .test(/\.(glb|gltf)$/) // 匹配 .glb 和 .gltf 文件
-        .use('file-loader') // 使用 file-loader
-        .loader('file-loader')
-        .options({
-          outputPath: 'models', // 输出到 models 文件夹
-          name: '[name].[hash].[ext]' // 文件名格式
-        });
+  publicPath: '/smart-coking-system-cn-blending/',
+  devServer: {
+    open: true,
+    port: 8888
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('glb') // 创建一个新的规则
+      .test(/\.(glb|gltf)$/) // 匹配 .glb 和 .gltf 文件
+      .use('file-loader') // 使用 file-loader
+      .loader('file-loader')
+      .options({
+        outputPath: 'models', // 输出到 models 文件夹
+        name: '[name].[hash].[ext]' // 文件名格式
+      });
     // 输出 Webpack 配置以便调试
     console.log(config.toConfig());
-    }
-  };
-  
+  }
+};
